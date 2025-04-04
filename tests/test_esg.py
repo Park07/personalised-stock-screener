e3e9ebfabff1d865a4cfd88c039aea78697beb34
james <z5419332@ad.unsw.edu.au>
2025-04-04 14:54:16 +1100

    trigger test run

--- tests/test_esg.py
+++ tests/test_esg.py
@@ -2,7 +2,7 @@ import requests
 BASE_URL = "http://35.169.25.122"
 # BASE_URL = "http://127.0.0.1:5000"
 def test_esg_indicators():
-    """Test the external ESG api from SIEERA"""
+    """Test the external ESG api from SIEERA!"""
     res = requests.get(f"{BASE_URL}/indicators_esg?tickers=AAPL")
     assert res.status_code == 200
     with open('tests/esg_test.json', 'r', encoding='utf-8') as file:
