564798e57d8fe9266e61e4235f9b57ae34d1f348
Minecraft Creeper <104009570+BreadSpriteBread@users.noreply.github.com>
2025-04-04 14:22:32 +1100

    tests (#24)
    * strategy.py first iteration, SMA crossover
    
    * updated requirements and gitignore
    
    * modified readme
    
    * modified readme
    
    * changed readme
    
    * gitignore, updated readme
    
    * fixed trailing whitepsace
    
    * optimised get_indicators path
    
    * updated json formatting
    
    * added crypto support for indicators
    
    * fixed bug in indicators with indicators having multiple arrays as outputs
    
    * added tests
    
    * linting
    
    * Linting
    
    * Fixing test and more liniting
    
    * Fixing linit
    
    * strategy modularisation
    
    * strategy modularisation renamed some stuff
    
    * added log file and additional logging functionality, added seperate path for crypto indicators, debugged all strategies, re did retrun dict datastructure
    
    * fixed some stuff in strategy
    
    * updated tests
    
    * Linting
    
    * More Linting
    
    * advice probability fully working
    
    * Linting
    
    * Linting
    
    * Fixing tests
    
    * Fixing tests linting
    
    * esg external api, H17B_SIERRA
    
    * accidentally added strategy_test deleting it
    
    * linting
    
    * Update esg.py
    
    * Update esg.py
    
    linting 2
    
    * added tests
    
    * tests
    
    * lint
    
    * Update esg.py
    
    * lint 2
    
    * fixed auto test test_esg.py
    
    * Update test_esg.py
    
    ---------
    
    Co-authored-by: jumer <jumer.azad@gmail.com>

--- src/esg.py
+++ src/esg.py
@@ -1,7 +1,5 @@
 import requests
-
 BASE_URL = "https://gh4vkppgue.execute-api.us-east-1.amazonaws.com/prod/api/esg/"
-
 def get_esg_indicators(tickers):
     res_dict = {}
     for ticker in tickers:
